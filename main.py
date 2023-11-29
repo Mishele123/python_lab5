4d04244a0b95bf8d8dddafc2198ea17c7d8a382d
Mishele123 <misha228665161@gmail.com>
2023-11-29 14:59:02 +0400

    return_lists function

--- main.py
+++ main.py
@@ -12,16 +12,31 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 import zipfile
+import glob
+from PIL import Image
+import random
 
 
-def get_list_dirs() -> None:
+
+
+def return_lists() -> list:
+    "the function returns an array containing the names of the images"
     train_dir = "train_dir"
-    print(os.listdir(train_dir))
+    test_dir = "test_dir"
+    validate_dir = "validate_dir"
 
+    train_list = glob.glob(os.path.join(train_dir,'*.jpg'))
+    test_list = glob.glob(os.path.join(test_dir,'*.jpg'))
+    validate_list = glob.glob(os.path.join(validate_dir,'*.jpg'))
+    
+
+    print(train_list[0].split("\\")[1].split("_")[0])
+
+    return train_list, test_list, validate_list
 
 
 def main() -> None:
-    get_list_dirs()
+    return_lists()
 
 if __name__ == "__main__":
     main()
